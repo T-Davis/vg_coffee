05d47d8e704ebf113d12ccabc274fe41d8142e0e
Trevor Davis <tzendou@gmail.com>
2024-10-11 11:43:20 -0600

    feat: add save and delete image functionality

--- packages/coffee_repository/lib/src/coffee_repository.dart
+++ packages/coffee_repository/lib/src/coffee_repository.dart
@@ -1,15 +1,22 @@
+import 'dart:typed_data';
+
 import 'package:alex_flipnote_coffee_api/alex_flipnote_coffee_api.dart';
 import 'package:coffee_repository/coffee_repository.dart';
+import 'package:local_storage_api/local_storage_api.dart';
 
 /// {@template coffee_repository}
-/// A Very Good Project created by Very Good CLI.
+/// Dart repository for managing coffee data.
 /// {@endtemplate}
 class CoffeeRepository {
   /// {@macro coffee_repository}
-  CoffeeRepository({AlexFlipnoteCoffeeApiClient? coffeeApiClient})
-      : _coffeeApiClient = coffeeApiClient ?? AlexFlipnoteCoffeeApiClient();
+  CoffeeRepository({
+    AlexFlipnoteCoffeeApiClient? coffeeApiClient,
+    LocalStorageApi? localStorageApi,
+  })  : _coffeeApiClient = coffeeApiClient ?? AlexFlipnoteCoffeeApiClient(),
+        _localStorageApi = localStorageApi ?? LocalStorageApi();
 
   final AlexFlipnoteCoffeeApiClient _coffeeApiClient;
+  final LocalStorageApi _localStorageApi;
 
   /// Returns a [CoffeeImage] fetched from the network.
   Future<CoffeeImage> fetchCoffeeImage() async {
@@ -18,5 +25,32 @@ class CoffeeRepository {
       filename: image.filename,
       bytes: image.bytes,
     );
+  }
+
+  /// Saves a [CoffeeImage] to local storage.
+  Future<void> saveCoffeeImage({
+    required Uint8List bytes,
+    required String filename,
+  }) async {
+    await _localStorageApi.saveImage(
+      bytes: bytes,
+      filename: filename,
+    );
+  }
+
+  /// Deletes a [CoffeeImage] from local storage.
+  Future<void> deleteCoffeeImage(String filename) async {
+    await _localStorageApi.deleteImage(filename);
+  }
+
+  /// Retrieves all [CoffeeImage]s from local storage.
+  Future<List<CoffeeImage>> getCoffeeImages() async {
+    final images = await _localStorageApi.getImages();
+    return images.map((image) {
+      return CoffeeImage(
+        filename: image.filename,
+        bytes: image.bytes,
+      );
+    }).toList();
   }
 }
--- packages/coffee_repository/test/src/coffee_repository_test.dart
+++ packages/coffee_repository/test/src/coffee_repository_test.dart
@@ -1,21 +1,33 @@
 import 'dart:typed_data';
 
 import 'package:alex_flipnote_coffee_api/alex_flipnote_coffee_api.dart';
+import 'package:alex_flipnote_coffee_api/src/models/image.dart' as a;
 import 'package:coffee_repository/coffee_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:local_storage_api/local_storage_api.dart';
+import 'package:local_storage_api/src/models/image.dart' as l;
 import 'package:mocktail/mocktail.dart';
-import 'package:test/test.dart';
 
 class MockAlexFlipnoteCoffeeApiClient extends Mock
     implements AlexFlipnoteCoffeeApiClient {}
 
+class MockLocalStorageApi extends Mock implements LocalStorageApi {}
+
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   group('CoffeeRepository', () {
     late AlexFlipnoteCoffeeApiClient coffeeApiClient;
+    late LocalStorageApi localStorageApi;
     late CoffeeRepository coffeeRepository;
 
     setUp(() {
       coffeeApiClient = MockAlexFlipnoteCoffeeApiClient();
-      coffeeRepository = CoffeeRepository(coffeeApiClient: coffeeApiClient);
+      localStorageApi = MockLocalStorageApi();
+      coffeeRepository = CoffeeRepository(
+        coffeeApiClient: coffeeApiClient,
+        localStorageApi: localStorageApi,
+      );
     });
 
     group('constructor', () {
@@ -25,29 +37,107 @@ void main() {
     });
 
     group('fetchCoffeeImage', () {
-      final image = Image(
-        imageType: 'png',
+      final image = a.Image(
+        filename: 'image.png',
         bytes: Uint8List.fromList([1, 2, 3]),
       );
 
       test('calls fetchRandomImage', () async {
         when(() => coffeeApiClient.fetchRandomImage())
             .thenAnswer((_) async => image);
-        try {
-          await coffeeRepository.fetchCoffeeImage();
-        } catch (_) {}
+
+        await coffeeRepository.fetchCoffeeImage();
+
         verify(() => coffeeApiClient.fetchRandomImage()).called(1);
       });
 
       test('returns correct data', () async {
         when(() => coffeeApiClient.fetchRandomImage())
             .thenAnswer((_) async => image);
+
         final actual = await coffeeRepository.fetchCoffeeImage();
+
         expect(
           actual,
           isA<CoffeeImage>()
-              .having((i) => i.imageType, 'imageType', 'png')
+              .having((i) => i.filename, 'filename', 'image.png')
               .having((i) => i.bytes, 'bytes', [1, 2, 3]),
+        );
+      });
+    });
+
+    group('saveCoffeeImage', () {
+      test('calls saveImage', () async {
+        const filename = 'test.png';
+        final bytes = Uint8List.fromList([1, 2, 3]);
+        when(
+          () => localStorageApi.saveImage(
+            bytes: bytes,
+            filename: filename,
+          ),
+        ).thenAnswer((_) async {});
+
+        await coffeeRepository.saveCoffeeImage(
+          bytes: bytes,
+          filename: filename,
+        );
+
+        verify(
+          () => localStorageApi.saveImage(
+            bytes: bytes,
+            filename: filename,
+          ),
+        ).called(1);
+      });
+    });
+
+    group('deleteCoffeeImage', () {
+      test('calls deleteImage', () async {
+        const filename = 'test.png';
+        when(() => localStorageApi.deleteImage(filename))
+            .thenAnswer((_) async {});
+
+        await coffeeRepository.deleteCoffeeImage(filename);
+
+        verify(() => localStorageApi.deleteImage(filename)).called(1);
+      });
+    });
+
+    group('getCoffeeImages', () {
+      final images = [
+        l.Image(
+          filename: 'test1.png',
+          bytes: Uint8List.fromList([1, 2, 3]),
+        ),
+        l.Image(
+          filename: 'test2.png',
+          bytes: Uint8List.fromList([1, 2, 3, 4]),
+        ),
+      ];
+
+      test('calls getImages', () async {
+        when(() => localStorageApi.getImages()).thenAnswer((_) async => images);
+
+        await coffeeRepository.getCoffeeImages();
+
+        verify(() => localStorageApi.getImages()).called(1);
+      });
+
+      test('returns correct data', () async {
+        when(() => localStorageApi.getImages()).thenAnswer((_) async => images);
+
+        final actual = await coffeeRepository.getCoffeeImages();
+
+        expect(
+          actual,
+          [
+            isA<CoffeeImage>()
+                .having((i) => i.filename, 'filename', 'test1.png')
+                .having((i) => i.bytes, 'bytes', [1, 2, 3]),
+            isA<CoffeeImage>()
+                .having((i) => i.filename, 'filename', 'test2.png')
+                .having((i) => i.bytes, 'bytes', [1, 2, 3, 4]),
+          ],
         );
       });
     });
